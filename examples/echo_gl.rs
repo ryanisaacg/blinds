c779cfdcd412a611a0113dda7fc884c44e8d92e7
John P Mayer, Jr <john.p.mayer.jr@gmail.com>
2020-01-14 18:21:46 -0500

    ensure examples are appropriately feature-guarded

--- examples/echo_gl.rs
+++ examples/echo_gl.rs
@@ -1,10 +1,14 @@
+#[cfg(feature = "gl")]
 use blinds::{run_gl, Event, EventStream, Key, Settings, Window};
+#[cfg(feature = "gl")]
 use glow::Context;
 
 fn main() {
+    #[cfg(feature = "gl")]
     run_gl(Settings::default(), app);
 }
 
+#[cfg(feature = "gl")]
 async fn app(_window: Window, _ctx: Context, mut events: EventStream<Event>) {
     'outer: loop {
         while let Some(ev) = events.next_event().await {
